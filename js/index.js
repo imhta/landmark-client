9b572c989072ecb0fc81b45ddd58e6dabb071d96
imhtop <nponmuthuselvam@gmail.com>
2019-07-01 22:18:35 +0530

    except slide bug home completed

--- js/index.js
+++ js/index.js
@@ -10,23 +10,14 @@ var $clientCarousel =   $('.client-carousel').flickity();
 
 var query = `query {
       configs(where: {
-      type: "logo"
+      type: "banner"
       }){
           id
           brandImg{
             handle
           }
         }
-    categories{
-          id
-          catType
-          title
-          img {
-            id
-            handle
-          }
-          catLocType
-        }
+
         clients{
           id
           name
@@ -35,16 +26,14 @@ var query = `query {
               handle
           }
       }
-      services{
-        id
+      firstSlides{
         title
-        description
         img{
           id
           handle
         }
       }
-      firstSlides{
+      secondSlides{
         title
         img{
           id
@@ -69,62 +58,31 @@ fetch('https://api-apeast.graphcms.com/v1/cjs3ei2im0dqf01g7re7ifxb7/master/graph
     console.log(res);
 
     createSlides(res.data.firstSlides);
-
+    createTopBanner(res.data.configs);
+    createSecondSlide(res.data.secondSlides);
+    createSideBanner(res.data.configs);
     createClients(res.data.clients);
-
+  
   });
 
+  // 
+function createTopBanner(config) {
+  for (i = 0; i < 3; i++) {
+    $("#top-banner").append(`
+    <div class="col-md" 
+         style="
+                background-image: url(https://media.graphcms.com/${config[i].brandImg.handle});
+                background-size: cover;
+                width: 100%;
+                height:300px ">
+    <div>
 
-
-function createLogo(config) {
-
-  $('#logo').append(`<a href="#intro"><img src="https://media.graphcms.com/${config[0].brandImg.handle}" alt="landmark logo" title="landmark since 1990" /></a>`)
-  $('#foot-logo').append(`<a href="#intro"><img src="https://media.graphcms.com/${config[0].brandImg.handle}" alt="landmark logo" title="landmark since 1990" /></a>`)
-}
-
-function createPortfolio(cats) {
-  var cards = document.createDocumentFragment();
-  uniqueArr = removeDups(cats.map((data) => data.title));
-  for (i = 0; i < uniqueArr.length; i++) {
-    var node = document.createElement("li");
-    var textnode = document.createTextNode(cats[i].title);
-    var att = document.createAttribute("data-filter");
-    att.value = `.${cats[i].catType}`;
-    node.className = `filter ${cats[i].catType}`;
-    node.appendChild(textnode);
-    node.setAttributeNode(att);
-    document.getElementById("portfolio-flters").appendChild(node);
-  }
-  for (i = 0; i < cats.length; i++) {
-
-    var cardDiv = document.createElement("div");
-    cardDiv.classList.add('col-lg-4', 'col-md-6', 'portfolio-item', cats[i].catType, 'wow', 'fadeInUp');
-
-    cardDiv.innerHTML = `
-
-    <div class="portfolio-wrap">
-      <figure>
-        <img src="https://media.graphcms.com/${cats[i].img.handle}" class="img-fluid" alt="">
-        <a href="https://media.graphcms.com/${cats[i].img.handle}" data-lightbox="portfolio" data-title="${cats[i].title}" class="link-preview"
-          title="Preview"><i class="ion ion-eye"></i></a>
-        <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a>
-      </figure>
- 
-      <div class="portfolio-info">
-        <h4><a href="#">${cats[i].title}</a></h4>
-        <p>${cats[i].catLocType}</p>
-      </div>
-    </div>
-
-    `;
-    cards.appendChild(cardDiv);
+    `);
 
   }
-  document.getElementById("portfolio-js").appendChild(cards);
-  var newItems = $('.portfolio-container').find('.portfolio-item');
-  $('.portfolio-container').isotope('insert', newItems);
 
 }
+
 
 function createClients(clients) {
   for (i = 0; i < clients.length; i++) {
@@ -138,22 +96,7 @@ function createClients(clients) {
   }
 }
 
-function createServices(services) {
-  for (i = 0; i < services.length; i++) {
 
-    $("#services-js").append(`
-    <div class="col-md-4 wow fadeInUp">
-    <div class="col ">
-        <img src="https://media.graphcms.com/${services[i].img.handle}" alt="${services[i].title}" class="img-fluid">
-      <h2 class="title m-2"><a href="#">${services[i].title}</a></h2>
-      <p class="m-2">${services[i].description}</p>
-    </div>
-    </div>
-    `);
-
-  }
-
-}
 
 
 function createSlides(slides) {
@@ -175,14 +118,33 @@ function createSlides(slides) {
   }
 }
 
-function removeDups(arr) {
-  let unique = {};
-  arr.forEach(function (i) {
-    if (!unique[i]) {
-      unique[i] = true;
-    }
-  });
-  return Object.keys(unique);
+
+function createSecondSlide(slides) {
+  for (i = 0; i < slides.length; i++) {
+    $("#second-slide-wrapper").append(`
+
+    <div>
+    <img data-u="image" src="https://media.graphcms.com/${slides[i].img.handle}" />
+    <img data-u="thumb" src="https://media.graphcms.com/${slides[i].img.handle}" style="height: 90px; width:190px"/>
+  </div>
+
+    `);
+
+  }
 }
 
+function createSideBanner(config) {
+  $("#home-side-banner")
+  .attr('style',
+    `max-width:100%;
+    max-height:100%;
+    background-image: url(https://media.graphcms.com/${config[3].brandImg.handle});
+    background-size: cover; `);
 
+    $("#final-home-banner").attr('style',
+    `max-width:100%;
+    max-height:100%;
+    margin: 10px;
+    background-image: url(https://media.graphcms.com/${config[4].brandImg.handle});
+    background-size: cover; `);
+}
